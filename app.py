e741691617d4cf09d6d10a98208c4257b0998931
Eugene702 <eugenefeilianputrarangga@gmail.com>
2024-06-22 04:31:15 +0700

    as

--- app.py
+++ app.py
@@ -1,31 +1,21 @@
-import os
-import joblib
 from fastapi import FastAPI
+import joblib
 from pydantic import BaseModel
 
 app = FastAPI()
 
-# Determine absolute path to models directory
-models_dir = os.path.join(os.getcwd(), 'models')
-
-# Load model and vectorizer using absolute paths
-model_path = os.path.join(models_dir, 'model_sentimen_analis.pkl')
-vectorizer_path = os.path.join(models_dir, 'vectorizer.pkl')
-
-model = joblib.load(model_path)
-vectorizer = joblib.load(vectorizer_path)
+# Load model and vectorizer
+model = joblib.load('./model_sentimen_analis.pkl')
+vectorizer = joblib.load('./vectorizer.pkl')
 
 class TextData(BaseModel):
     text: str
 
 @app.post("/predict")
 async def predict(data: TextData):
-    try:
-        # Transform the input text
-        X = vectorizer.transform([data.text])
-        prediction = model.predict(X)
-        # Convert numpy.int64 to int
-        prediction_int = int(prediction[0])
-        return {"text": data.text, "prediction": prediction_int}
-    except Exception as e:
-        return {"error": str(e)}
+    # Preprocess the input text
+    X = vectorizer.transform([data.text])
+    prediction = model.predict(X)
+    # Convert numpy.int64 to int
+    prediction_int = int(prediction[0])
+    return {"text": data.text, "prediction": prediction_int}
